f08cd4a37945f8bf446a6b5a1b4da8ad15efcef5
Kriti <kriti@Kriti.local>
2019-04-21 20:47:07 +0530

    readme file updated

--- carousel-poc/src/constants/index.js
+++ carousel-poc/src/constants/index.js
@@ -5,6 +5,6 @@ export const IMAGES_URL = 'https://pixabay.com/api/?key=';
 // Breakpoints for responsiveness
 export const MOBILE_MAX_WIDTH = 480;
 
-// No. of slides in to be shown in carousel in desktop
+// No. of slides to be shown in carousel in desktop
 export const SLIDES_COUNT_IN_DESKTOP = 5;
 
